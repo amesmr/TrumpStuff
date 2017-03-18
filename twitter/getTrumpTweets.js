fc9feb1a67e495d7c2a777e2a675fa7e5a8018e0
James <jwin4740@gmail.com>
2017-03-18 18:02:09 -0400

    added twitter js files

---
+++ twitter/getTrumpTweets.js
@@ -0,0 +1,25 @@
+
+
+getMyTweets();
+
+function getMyTweets() {
+    var twitterObj = require("./keys.js");
+    var Twitter = require('twitter');
+    var twitterCreds = twitterObj.twitterKeys;
+    var user = new Twitter(twitterCreds);
+    var params = { screen_name: '@realDonaldTrump' };
+    user.get('statuses/user_timeline.json?screen_name=twitterapi&count=400', params, function(error, tweets, response) {
+        if (error) throw error;
+        // if no error occurs a for loop will console log each tweet
+
+        // call the function which logs the command and timestamp to log.txt file
+        var n = tweets.length;
+        for (var i = 0; i < n; i++) {
+            console.log("\n" + i + ".." + tweets[i].text + "\n");
+        }
+
+    });
+}
+
+
+
---
+++ twitter/keys.js
@@ -0,0 +1,9 @@
+
+// this file contains all the keys and authorizations to access the twitter API
+// the object is exported for use in the main liri.js file
+exports.twitterKeys = {
+  consumer_key: 's0cR81GiPfDqpJbGMNjXt3IL9',
+  consumer_secret: '82NSqEbfJkCNWaMvwI8Zvx1BJ0Do5yyVdwpdYOGGWucTX7IfUg',
+  access_token_key: '830588118864691200-hzKtuXbFpfLxSeDajq1Y598bVM02B9l',
+  access_token_secret: 'bafw09CwH5Sl5KV6lkjXfcieUGoTG090C4A1xDsx5xQO0',
+}
