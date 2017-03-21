412a011cb96170585655a575da5b10ff2b121931
matt ames <matt ames>
2017-03-20 23:24:02 -0400

    updated DB to include ratings for the quotes

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,13 @@
---Create the database event_saver_db and specified it for use.
+-- Create the database event_saver_db and specified it for use.
 CREATE DATABASE trump_quotes_db;
 USE trump_quotes_db;
 
---Create the table events.
-CREATE TABLE quotes
-    (
-        id int NOT NULL AUTO_INCREMENT,
-        quote varchar(800) NOT NULL,
-        source varchar(255) NOT NULL,
-        year integer(4),
-        PRIMARY KEY(id)
-    );
+-- Create the table events.
+CREATE TABLE quotes (
+    id INT NOT NULL AUTO_INCREMENT,
+    quote VARCHAR(800) NOT NULL,
+    rating VARCHAR(6) NOT NULL,
+    source VARCHAR(255) NOT NULL,
+    year YEAR(4) DEFAULT NULL,
+    PRIMARY KEY (id)
+);
