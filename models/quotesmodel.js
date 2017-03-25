51e4e6e7f7654aa4a82292833b013c87dc2b8904
matt ames <matt ames>
2017-03-25 14:34:26 -0400

    updating new routes  and corrected schema

---
+++ models/quotesmodel.js
@@ -0,0 +1,34 @@
+module.exports = function(sequelize, DataTypes) {
+  var Quotes = sequelize.define("quotes", {
+    quote: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    source: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+          len: [1]
+      }
+    },
+    rating: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
+        }
+    },
+    year: {
+        type: DataTypes.DATEONLY
+    }
+  }, {
+          timestamps: false,
+          freezeTableName: true,
+          tableName: 'quotes'
+
+      });
+  return Quotes;
+};
