e4b6e3a2ad79c0bb52f43d8a48735a0b1f66504e
James <jwin4740@gmail.com>
2017-03-29 12:17:12 -0400

    constructors

--- public/js/graphforce.js
+++ public/js/graphforce.js
@@ -1,341 +1,340 @@
- var parsedData = {
-     nodes: [{
+  // constructor Link function
+  function NodeConstruct(name, color, size, layer, linkType, index) {
+      this.name = name;
+      this.color = color;
+      this.size = size;
+      this.layer = layer;
+      this.linkType = linkType;
+      this.index = index;
 
-             name: "ROOT",
-             color: "white",
-             size: 20,
-             layer: 0,
-             index: 0
-
-         },
-
-         // main branches
-         {
-             name: "himself",
-             color: "red",
-             size: 10,
-             layer: 1,
-             index: 1
+  }
 
 
-         },
-         {
-             name: "defenses",
-             color: "green",
-             size: 10,
-             layer: 1,
-             index: 2
+  // constructor Link function
+  function LinkConstruct(source, target, sourceName, targetName, linkColor, distance) {
+      this.source = source;
+      this.target = target;
+      this.sourceName = sourceName;
+      this.targetName = targetName;
+      this.linkColor = linkColor;
+      this.distance = distance;
+  }
 
-         },
-         {
-             name: "celebrity",
-             color: "yellow",
-             size: 10,
-             layer: 1,
-             index: 3
+//   function createNodesAndLinks() {
 
-         }, {
-             name: "debates",
-             color: "brown",
-             size: 10,
-             layer: 1,
-             index: 4
+//   }
+  var parsedData = {
+      nodes: [{
 
-         }, {
-             name: "clintons",
-             color: "grey",
-             size: 10,
-             layer: 1,
-             index: 5
+              name: "ROOT",
+              color: "white",
+              size: 20,
+              layer: 0,
+              index: 0
 
-         }, {
-             name: "nicknames",
-             color: "lawngreen",
-             size: 10,
-             layer: 1,
-             index: 6
+          },
 
-         }, {
-             name: "one liners",
-             color: "blue",
-             size: 10,
-             layer: 1,
-             index: 7
-
-         },
-
-         // level 2 branches
-
-         {
-             name: "Little Marco",
-             color: "violet",
-             size: 5,
-             layer: 2,
-             index: 8
-
-         }, {
-             name: "Lyin Ted",
-             color: "violet",
-             size: 5,
-             layer: 2,
-             index: 9
-
-         }, {
-             name: "Crooked Hillary",
-             color: "violet",
-             size: 5,
-             layer: 2,
-             index: 10
-
-         }, {
-             name: "Low energy Jeb",
-             color: "violet",
-             size: 5,
-             layer: 2,
-             index: 11
-
-         },
-         // third layer
-         {
-             name: "jeb off stage",
-             color: "green",
-             size: 5,
-             layer: 3,
-             index: 12
-
-         }, {
-             name: "tough guy",
-             color: "green",
-             size: 5,
-             layer: 3,
-             index: 13
-
-         }, {
-             name: "real tough guy",
-             color: "green",
-             size: 5,
-             layer: 3,
-             index: 14
-         }
-     ],
-     links: [{
-             source: 0,
-             target: 1,
-             value: 4,
-             distance: 100
-         },
-         {
-             source: 0,
-             target: 2,
-             value: 4,
-             distance: 100
-         },
-         {
-             source: 0,
-             target: 3,
-             value: 4,
-             distance: 100
-         }, {
-             source: 0,
-             target: 4,
-             value: 4,
-             distance: 100
-         }, {
-             source: 0,
-             target: 5,
-             value: 4,
-             distance: 100
-         }, {
-             source: 0,
-             target: 6,
-             value: 4,
-             distance: 100
-         }, {
-             source: 0,
-             target: 7,
-             value: 4,
-             distance: 100
-         }, {
-             source: 6,
-             target: 8,
-             value: 4,
-             distance: 75
-         }, {
-             source: 6,
-             target: 9,
-             value: 4,
-             distance: 75
-         }, {
-             source: 6,
-             target: 10,
-             value: 4,
-             distance: 75
-         }, {
-             source: 6,
-             target: 11,
-             value: 4,
-             distance: 75
-         }, {
-             source: 11,
-             target: 12,
-             value: 4,
-             distance: 75
-         }, {
-             source: 11,
-             target: 13,
-             value: 4,
-             distance: 75
-         }, {
-             source: 11,
-             target: 14,
-             value: 4,
-             distance: 75
-         },
-         {
-             source: 12,
-             target: 4,
-             value: 4,
-             distance: 75
-         }
-     ]
- };
- var width = 1200;
- var height = 700;
-
- var svg = d3.select("#force")
-     .attr("width", width)
-     .attr("height", height);
-
- var simulation = d3.forceSimulation()
-     .force("link", d3.forceLink().distance(function (d) {
-         return d.distance;
-     }).strength(2))
-     //  .force("center", d3.forceCenter(width / 2, height / 2))
-     .force("charge", d3.forceManyBody());
+          // main branches
+          {
+              name: "himself",
+              color: "red",
+              size: 10,
+              layer: 1,
+              index: 1
 
 
- simulation
-     .nodes(parsedData.nodes);
+          },
+          {
+              name: "defenses",
+              color: "green",
+              size: 10,
+              layer: 1,
+              index: 2
 
- simulation
-     .force("link")
-     .links(parsedData.links);
+          },
+          {
+              name: "celebrity",
+              color: "yellow",
+              size: 10,
+              layer: 1,
+              index: 3
 
- var link = svg.selectAll(".link")
-     .data(parsedData.links)
-     .enter().append("line")
-     .attr("class", "link");
+          }, {
+              name: "debates",
+              color: "brown",
+              size: 10,
+              layer: 1,
+              index: 4
+
+          }, {
+              name: "clintons",
+              color: "grey",
+              size: 10,
+              layer: 1,
+              index: 5
+
+          }, {
+              name: "nicknames",
+              color: "lawngreen",
+              size: 10,
+              layer: 1,
+              index: 6
+
+          },
+
+          // level 2 branches
+
+          {
+              name: "Little Marco",
+              color: "violet",
+              size: 5,
+              layer: 2,
+              index: 7
+
+          }, {
+              name: "Lyin Ted",
+              color: "violet",
+              size: 5,
+              layer: 2,
+              index: 8
+
+          }, {
+              name: "Crooked Hillary",
+              color: "violet",
+              size: 5,
+              layer: 2,
+              index: 9
+
+          }, {
+              name: "Low energy Jeb",
+              color: "violet",
+              size: 5,
+              layer: 2,
+              index: 10
+
+          },
+          // third layer
+          {
+              name: "jeb off stage",
+              color: "green",
+              size: 5,
+              layer: 3,
+              index: 11
+
+          }, {
+              name: "tough guy",
+              color: "green",
+              size: 5,
+              layer: 3,
+              index: 12
+
+          }, {
+              name: "real tough guy",
+              color: "green",
+              size: 5,
+              layer: 3,
+              index: 13
+          }
+      ],
+      links: [{
+              source: 0,
+              target: 1,
+              sourceText: "root",
+              targetText: "himself",
+              linkType: "main",
+              distance: 100
+          }
+          //  {
+          //      source: 0,
+          //      target: 2,
+          //      distance: 100
+          //  },
+          //  {
+          //      source: 0,
+          //      target: 3,
+          //      distance: 100
+          //  }, {
+          //      source: 0,
+          //      target: 4,
+          //      distance: 100
+          //  }, {
+          //      source: 0,
+          //      target: 5,
+          //      distance: 100
+          //  }, {
+          //      source: 0,
+          //      target: 6,
+          //      distance: 100
+          //  }, {
+          //      source: 0,
+          //      target: 7,
+          //      distance: 100
+          //  }, {
+          //      source: 6,
+          //      target: 8,
+          //      distance: 75
+          //  }, {
+          //      source: 6,
+          //      target: 9,
+          //      distance: 75
+          //  }, {
+          //      source: 6,
+          //      target: 10,
+          //      distance: 75
+          //  }, {
+          //      source: 6,
+          //      target: 11,
+          //      distance: 75
+          //  }, {
+          //      source: 11,
+          //      target: 12,
+          //      distance: 75
+          //  }, {
+          //      source: 11,
+          //      target: 13,
+          //      distance: 75
+          //  }
+      ]
+  };
 
 
- var node = svg.selectAll(".node")
-     .data(parsedData.nodes)
-     .enter().append("g")
-     .attr("class", "node")
-     .call(d3.drag()
-         .on("start", dragstarted)
-         .on("drag", dragged)
-         .on("end", dragended));
+  var width = 1200;
+  var height = 700;
+
+  var svg = d3.select("#force")
+      .attr("width", width)
+      .attr("height", height);
+
+  var simulation = d3.forceSimulation()
+      .force("link", d3.forceLink().distance(function (d) {
+          return d.distance;
+      }).strength(2))
+      //  .force("center", d3.forceCenter(width / 2, height / 2))
+      .force("charge", d3.forceManyBody());
 
 
- node.append("circle")
-     .attr("r", function (d) {
-         if (d.size > 19) {
-             return 8;
-         } else {
-             return 3;
-         }
-     })
-     .style("fill", function (d) {
-         return d.color;
-     });
+  simulation
+      .nodes(parsedData.nodes);
 
- // .attr("x", -8)
- // .attr("y", -8)
- // .attr("width", 16)
- // .attr("height", 16);
+  simulation
+      .force("link")
+      .links(parsedData.links);
 
- node.append("text")
-     .attr("dx", 12)
-     .attr("dy", ".35em")
-     .text(function (d) {
-         var str = d.name;
-         return str.toUpperCase();
-     })
-     .style("font-size", function (d) {
-         switch (d.layer) {
-             case 0:
-                 return "16px";
-                 break;
-             case 1:
-                 return "11px";
-                 break;
-             case 2:
-                 return "9px";
-                 break;
-             case 3:
-                 return "9px";
-                 break;
-         }
-     })
-     .style("fill", "white");
-
- simulation.on("tick", function () {
-     link.attr("x1", function (d) {
-             return d.source.x;
-         })
-         .attr("y1", function (d) {
-             return d.source.y;
-         })
-         .attr("x2", function (d) {
-             return d.target.x;
-         })
-         .attr("y2", function (d) {
-             return d.target.y;
-         });
-
-     node.attr("transform", function (d) {
-         return "translate(" + d.x + "," + d.y + ")";
-     });
- });
+  var link = svg.selectAll(".link")
+      .data(parsedData.links)
+      .enter().append("line")
+      .attr("class", "link");
 
 
- function ticked() {
-     link
-         .attr("x1", function (d) {
-             return d.source.x;
-         })
-         .attr("y1", function (d) {
-             return d.source.y;
-         })
-         .attr("x2", function (d) {
-             return d.target.x;
-         })
-         .attr("y2", function (d) {
-             return d.target.y;
-         });
+  var node = svg.selectAll(".node")
+      .data(parsedData.nodes)
+      .enter().append("g")
+      .attr("class", "node")
+      .call(d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended));
 
-     node
-         .attr("cx", function (d) {
-             return d.x;
-         })
-         .attr("cy", function (d) {
-             return d.y;
-         });
- }
 
- function dragstarted(d) {
-     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-     d.fx = d.x;
-     d.fy = d.y;
- }
+  node.append("circle")
+      .attr("r", function (d) {
+          if (d.size > 19) {
+              return 8;
+          } else {
+              return 5;
+          }
+      })
+      .style("fill", function (d) {
+          return d.color;
+      });
 
- function dragged(d) {
-     d.fx = d3.event.x;
-     d.fy = d3.event.y;
- }
+  // .attr("x", -8)
+  // .attr("y", -8)
+  // .attr("width", 16)
+  // .attr("height", 16);
 
- function dragended(d) {
-     if (!d3.event.active) simulation.alphaTarget(0);
-     d.fx = null;
-     d.fy = null;
- }
+  node.append("text")
+      .attr("dx", 12)
+      .attr("dy", ".35em")
+      .text(function (d) {
+          var str = d.name;
+          return str.toUpperCase();
+      })
+      .style("font-size", function (d) {
+          switch (d.layer) {
+              case 0:
+                  return "16px";
+                  break;
+              case 1:
+                  return "11px";
+                  break;
+              case 2:
+                  return "9px";
+                  break;
+              case 3:
+                  return "9px";
+                  break;
+          }
+      })
+      .style("fill", "white");
+
+  simulation.on("tick", function () {
+      link.attr("x1", function (d) {
+              return d.source.x;
+          })
+          .attr("y1", function (d) {
+              return d.source.y;
+          })
+          .attr("x2", function (d) {
+              return d.target.x;
+          })
+          .attr("y2", function (d) {
+              return d.target.y;
+          });
+
+      node.attr("transform", function (d) {
+          return "translate(" + d.x + "," + d.y + ")";
+      });
+  });
+
+
+  function ticked() {
+      link
+          .attr("x1", function (d) {
+              return d.source.x;
+          })
+          .attr("y1", function (d) {
+              return d.source.y;
+          })
+          .attr("x2", function (d) {
+              return d.target.x;
+          })
+          .attr("y2", function (d) {
+              return d.target.y;
+          });
+
+      node
+          .attr("cx", function (d) {
+              return d.x;
+          })
+          .attr("cy", function (d) {
+              return d.y;
+          });
+  }
+
+  function dragstarted(d) {
+      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+  }
+
+  function dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+  }
+
+  function dragended(d) {
+      if (!d3.event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+  }
